ea529a4f2f8f61fa7efb956246a8571f67ea3989
Erick Allan Silva <98674896+erickallandev@users.noreply.github.com>
2022-06-18 21:52:24 -0300

    feat: almost finished, missing project photos

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,27 @@ import InstagramIcon from './assets/images/icons/icon4.png'
 import EmailIcon from './assets/images/icons/icon7.png'
 import LinkedinIcon from './assets/images/icons/icon11.png'
 import GitHubIcon from './assets/images/icons/icon10.png'
+import HTMLIcon from './assets/images/icons/icon12.png'
+import CSSIcon from './assets/images/icons/icon13.png'
+import BootstrapIcon from './assets/images/icons/icon14.png'
+import JSIcon from './assets/images/icons/icon15.png'
+import TSIcon from './assets/images/icons/icon16.png'
+import ReactJSIcon from './assets/images/icons/icon17.png'
+import SassIcon from './assets/images/icons/icon18.png'
+import FigmaIcon from './assets/images/icons/icon19.png'
+import GitIcon from './assets/images/icons/icon20.png'
 import UpArrowIcon from './assets/images/icons/up-arrow.png'
+import LeftArrowIcon from './assets/images/icons/left-arrow.png'
+import CircleIcon from './assets/images/icons/circle.png'
+import SquareIcon from './assets/images/icons/square.png'
+import LeftIcon from './assets/images/icons/left-arrow2.png'
+import CamIcon from './assets/images/icons/cam.png'
+import TelIcon from './assets/images/icons/phone.png'
+import MoreIcon from './assets/images/icons/more.png'
+import MicIcon from './assets/images/icons/mic.png'
+import SmileIcon from './assets/images/icons/emojiicon.png'
+import ClipIcon from './assets/images/icons/clipicon.png'
+import CameraIcon from './assets/images/icons/camera.png'
 import imageHome1 from './assets/images/image2.svg';
 import { useState, useEffect } from 'react';
 
@@ -38,11 +58,9 @@ const App = () => {
                         <C.logoImg scrollChanges={scrollChanges} src={logoPng}></C.logoImg>
                     </C.logo>
                     <C.Buttons scrollChanges={scrollChanges}>
-                        <C.ButtonMenu>Início</C.ButtonMenu>
-                        <C.ButtonMenu onClick={() => window.scrollTo(0, 900)}>Sobre</C.ButtonMenu>
-                        <C.ButtonMenu onClick={() => window.scrollTo(0, 1662)}>Serviços</C.ButtonMenu>
-                        <C.ButtonMenu onClick={() => window.scrollTo(0, 2421)}>Portfólio</C.ButtonMenu>
-                        <C.ButtonMenu onClick={() => window.scrollTo(0, 3183)}>Contato</C.ButtonMenu>
+                        <C.ButtonMenu onClick={() => window.scrollTo(0, 1107.1999)}>Sobre</C.ButtonMenu>
+                        <C.ButtonMenu onClick={() => window.scrollTo(0, 2036.8)}>Portfólio</C.ButtonMenu>
+                        <C.ButtonMenu onClick={() => window.scrollTo(0, 4396)}>Contato</C.ButtonMenu>
                     </C.Buttons>
                 </C.Mainbar>
                 <C.BodyContent>
@@ -68,19 +86,186 @@ const App = () => {
                     </C.Sidebar>
                     <C.Content>
                         <C.UpArrow onClick={() => window.scrollTo(0, 0)} scrollChanges={scrollChanges}>
-                            <C.UpArrowImg src={UpArrowIcon}/>
+                            <C.UpArrowImg src={UpArrowIcon} />
                         </C.UpArrow>
                         <C.ContentHome>
                             <C.image1 src={imageHome1}></C.image1>
-                            <C.saudacao>Olá! Eu sou o Erick, desenvolvedor WEB.</C.saudacao>
+                            <C.Typing>EA Web Development</C.Typing>
                         </C.ContentHome>
-                        <C.ContentAbout>bbbbb</C.ContentAbout>
-                        <C.ContentServices>ccccc</C.ContentServices>
-                        <C.ContentPortfolio>ddddd</C.ContentPortfolio>
-                        <C.ContentContact>eeeee</C.ContentContact>
+                        <C.ContentAbout>
+                            <C.Typing>
+                                Olá! Eu sou o Erick Allan.
+                            </C.Typing>
+                            <C.CompText>
+                                Sou um entusiasta do uso de tecnologias em geral, especialmente para resolver problemas cotidianos. O desenvolvimento de websites e sistemas Web tem o poder de impusionar os mais variados tipos de negócios do mercado. Quer impulsionar o seu negócio? Precisa expandir a visibilidade dos seus produtos? Precisa atrair mais clientes? Tudo isso pode ser alcançado através do uso de tecnologias Web, e eu estou aqui para te ajudar com isso.
+                            </C.CompText>
+                            <C.CompText>
+                                Atualmente eu desenvolvo utilizando tecnologias como HTML5, CSS3, Bootstrap, Javascript, Typescript, ReactJS, Tailwind CSS, CSS Modules, Styled Components e SASS. Também utilizo Whimsical e Figma para o planejamento e desenho dos projetos, além de Git e GitHub para versionamento de código.
+                            </C.CompText>
+                            <C.AboutLogos>
+                                <C.AboutLogoCircle>
+                                    <C.AboutLogoImg src={HTMLIcon} alt='' />
+                                </C.AboutLogoCircle>
+                                <C.AboutLogoCircle>
+                                    <C.AboutLogoImg src={CSSIcon} alt='' />
+                                </C.AboutLogoCircle>
+                                <C.AboutLogoCircle>
+                                    <C.AboutLogoImg src={BootstrapIcon} alt='' />
+                                </C.AboutLogoCircle>
+                                <C.AboutLogoCircle>
+                                    <C.AboutLogoImg src={JSIcon} alt='' />
+                                </C.AboutLogoCircle>
+                            </C.AboutLogos>
+                            <C.AboutLogos>
+                                <C.AboutLogoCircle>
+                                    <C.AboutLogoImg src={TSIcon} alt='' />
+                                </C.AboutLogoCircle>
+                                <C.AboutLogoCircle>
+                                    <C.AboutLogoImg src={ReactJSIcon} alt='' />
+                                </C.AboutLogoCircle>
+                                <C.AboutLogoCircle>
+                                    <C.AboutLogoImg src={FigmaIcon} alt='' />
+                                </C.AboutLogoCircle>
+                                <C.AboutLogoCircle>
+                                    <C.AboutLogoImg src={SassIcon} alt='' />
+                                </C.AboutLogoCircle>
+                                <C.AboutLogoCircle>
+                                    <C.AboutLogoImg src={GitIcon} alt='' />
+                                </C.AboutLogoCircle>
+                            </C.AboutLogos>
+                        </C.ContentAbout>
+
+                        <C.ContentPortfolio>
+                            <C.Typing>
+                                Projetos desenvolvidos
+                            </C.Typing>
+                            <C.DevelopedProject>
+                                <C.ProjectScreen>
+                                    Photo
+                                </C.ProjectScreen>
+                                <C.ProjectDescription>
+                                    <C.ProjectTitle>
+                                        To-Do List
+                                    </C.ProjectTitle>
+                                    <C.ProjectDescriptionText>
+                                        Projeto de to-do list desenvolvido com ReactJS. Através da aplicação web é possível adicionar novos ítens à lista, marcá-los como feito, desmarcá-los ou excluí-los. Essa to-do list pode ser útil para listar as atividades pendentes do dia e controlá-las à medida que forem sendo concluídas.
+                                    </C.ProjectDescriptionText>
+                                    <C.ProjectUsedTechnologies>
+                                        HTML5, CSS3, ReactJS, Styled-Components.
+                                    </C.ProjectUsedTechnologies>
+                                </C.ProjectDescription>
+                            </C.DevelopedProject>
+                            <C.ButtonNextProject onClick={() => window.scrollTo(0, 2840)}>Próximo projeto</C.ButtonNextProject>
+
+                            <C.ButtonPreviousProject onClick={() => window.scrollTo(0, 2036.8)}>Projeto anterior</C.ButtonPreviousProject>
+                            <C.DevelopedProject>
+                                <C.ProjectScreen>
+                                    Photo
+                                </C.ProjectScreen>
+                                <C.ProjectDescription>
+                                    <C.ProjectTitle>
+                                        Centrifuge Calculator
+                                    </C.ProjectTitle>
+                                    <C.ProjectDescriptionText>
+                                        Centrifuge Calculator é uma calculadora de parâmetros que geralmente são utilizados em centrífugas de laboratórios. É comum que seja necesário converter parâmetros utilizando o raio do rotor da centrífuga, além da força centrífuga e a quantidade de rotações por minuto adotadas em seu uso. Dessa forma, inserindo dois desses três parâmetros, pode-se obter facilmente o terceiro parâmetro, de forma simples, rápida, e confiável.
+                                    </C.ProjectDescriptionText>
+                                    <C.ProjectUsedTechnologies>
+                                        HTML5, CSS3, Javascript, Bootstrap.
+                                    </C.ProjectUsedTechnologies>
+                                </C.ProjectDescription>
+                            </C.DevelopedProject>
+                            <C.ButtonNextProject onClick={() => window.scrollTo(0, 3595.1999)}>Próximo projeto</C.ButtonNextProject>
+
+                            <C.ButtonPreviousProject onClick={() => window.scrollTo(0, 2840)}>Projeto anterior</C.ButtonPreviousProject>
+                            <C.DevelopedProject>
+                                <C.ProjectScreen>
+                                    Photo
+                                </C.ProjectScreen>
+                                <C.ProjectDescription>
+                                    <C.ProjectTitle>
+                                        Fictícia Technologies
+                                    </C.ProjectTitle>
+                                    <C.ProjectDescriptionText>
+                                        Trata-se do website de uma empresa fictícia de nome "Fictícia", do ramo de tecnologias. O site foi desenvolvido para fins de treinamento e aprendizado sobre a atualização do Bootstrap. 
+                                    </C.ProjectDescriptionText>
+                                    <C.ProjectUsedTechnologies>
+                                        HTML5, CSS3, Javascript, Bootstrap.
+                                    </C.ProjectUsedTechnologies>
+                                </C.ProjectDescription>
+                            </C.DevelopedProject>
+
+                        </C.ContentPortfolio>
+                        <C.ContentContact>
+                            <C.Typing>
+                                Vamos conversar sobre o seu projeto?
+                            </C.Typing>
+                            <C.linkWhatsapp href='https://wa.me/5584996501098' rel="noreferrer" target='_blank'>
+                                <C.CompContato>
+                                    <C.BarraSup>
+                                        <C.CompartSup>
+                                            <C.SupIconDiv>
+                                                <C.SupIconImg src={LeftIcon} />
+                                            </C.SupIconDiv>
+                                            <C.SupLogoDiv>
+                                                <C.SupLogoImg src={logoPng} />
+                                            </C.SupLogoDiv>
+                                            <C.SupTextDiv>
+                                                <C.SupTextbold>EAWebDev</C.SupTextbold>
+                                                <C.SupTextNormal>Online</C.SupTextNormal>
+                                            </C.SupTextDiv>
+                                        </C.CompartSup>
+                                        <C.CompartSup>
+                                            <C.SupIconDiv>
+                                                <C.SupIconImg src={CamIcon} />
+                                            </C.SupIconDiv>
+                                            <C.SupIconDiv>
+                                                <C.SupIconImg src={TelIcon} />
+                                            </C.SupIconDiv>
+                                            <C.SupIconDiv>
+                                                <C.SupIconImg src={MoreIcon} />
+                                            </C.SupIconDiv>
+                                        </C.CompartSup>
+                                    </C.BarraSup>
+                                    <C.TelaConv>
+                                        <C.TelaCompSup>
+                                            <C.telaConvTexto>Olá! Tudo bem? Clique aqui para entrar em contato via Whatsapp.</C.telaConvTexto>
+                                            <C.TelaConvHora>19:05</C.TelaConvHora>
+                                        </C.TelaCompSup>
+                                        <C.TelaCompInf>
+                                            <C.TelaInfType>
+                                                <C.iconType>
+                                                    <C.iconTypeImg src={SmileIcon} />
+                                                </C.iconType>
+                                                <C.textType>Digite sua mensagem</C.textType>
+                                                <C.iconType>
+                                                    <C.iconTypeImg src={ClipIcon} />
+                                                </C.iconType>
+                                                <C.iconType>
+                                                    <C.iconTypeImg src={CameraIcon} />
+                                                </C.iconType>
+                                            </C.TelaInfType>
+                                            <C.TelaInfRecord>
+                                                <C.recordImg src={MicIcon} />
+                                            </C.TelaInfRecord>
+                                        </C.TelaCompInf>
+                                    </C.TelaConv>
+                                    <C.BarraInf>
+                                        <C.divButtons>
+                                            <C.imgButtons src={LeftArrowIcon} alt='' />
+                                        </C.divButtons>
+                                        <C.divButtons>
+                                            <C.imgButtons src={CircleIcon} alt='' />
+                                        </C.divButtons>
+                                        <C.divButtons>
+                                            <C.imgButtons src={SquareIcon} alt='' />
+                                        </C.divButtons>
+                                    </C.BarraInf>
+                                </C.CompContato>
+                            </C.linkWhatsapp>
+                        </C.ContentContact>
                     </C.Content>
                 </C.BodyContent>
-                <C.Footer> Copyright © 2022 EA WebDev - Desenvolvido por Erick Allan Silva </C.Footer>
+                <C.Footer> Copyright © 2022 - EA WebDev </C.Footer>
             </C.Screen>
         </C.Container>
     );
--- src/styled.ts
+++ src/styled.ts
@@ -1,7 +1,8 @@
 import styled from 'styled-components';
+import whatsbg from './assets/images/whatsbg.png';
 
 type Props = {
-    scrollChanges: boolean
+    scrollChanges: boolean;
 }
 
 export const Container = styled.div`
@@ -90,10 +91,10 @@ export const UpArrow = styled.div(({scrollChanges}:Props) => (`
     justify-content: center;
     align-items: center;
     position: fixed;    
-    width: 80px;
-    height: 80px;
+    width: 60px;
+    height: 60px;
     background-color: rgba(255, 255, 255, 0.5);
-    border-radius: 80px;
+    border-radius: 30px;
     bottom: 30px;
     right: 30px;
     cursor: pointer;
@@ -112,12 +113,14 @@ export const MediaIcon = styled.div(({scrollChanges}:Props) => (`
     margin-left: auto;
     margin-right: auto;
     cursor: pointer;
+    margin-top: 5px;
+    margin-bottom: 5px;
     transition: 0.3s;
         :last-child {
             padding-bottom: 30px;
         }
         :hover {
-            transform: scale(1.5);
+            transform: scale(2);
             margin-bottom: 15px;
             margin-top: 15px;
         }
@@ -149,6 +152,7 @@ export const ContentHome = styled.div`
     height: 100vh;
     width: 100%;
     color: #f9a826;
+    padding-bottom: 200px;
 `;
 
 export const image1 = styled.img`
@@ -157,55 +161,413 @@ export const image1 = styled.img`
 `;
 
 export const ContentAbout = styled.div`
-    display: block;
+    display: flex;
+    flex-direction: column;
     height: 100vh;
     width: 100%;
-    background-color: #FFF;
-    color: #000F;
-    background-color: #00f;
+    padding-bottom: 200px;
+    text-align: center;
+    align-items: center;
 `;
 
-export const ContentServices = styled.div`
-    display: block;
-    height: 100vh;
+export const CompHeader = styled.h2`~
+    text-align: center;
+    margin: 0 50px;
+    color: #f9a826;
+    font-size: 50px;
+    text-align: center;
+    padding-top: 50px;
+    padding-bottom: 50px;
+`;
+
+export const CompText = styled.p`
+    text-align: justify;
+    margin: 25px 50px;
+    margin-top: 0;
+    color: #FFFFFF;
+    font-size: 20px;
+`;
+
+export const AboutLogos = styled.div`
     width: 100%;
-    color: #000F;
-    background-color: #cf4;
+    display: flex;
+    justify-content: center;
+    padding-top: 20px;
+`;
+
+export const AboutLogoCircle = styled.div`
+    width: 100px;
+    height: 100px;
+    border-radius: 50px;
+    background-color: #FFF;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin: 5px 20px;
+    cursor: pointer;
+        :hover {
+            box-shadow: 0 0 50px #FFFFFF;
+        }
+`;
+
+export const AboutLogoImg = styled.img`
+    width: 50px;
+`;
+
+export const DevelopedProject = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
+
+export const ButtonNextProject = styled.div`
+    background-color: #00000f;
+    border: 2px solid #FFF;
+    color: #FFF;
+    padding 20px 40px;
+    border-radius: 30px;
+    margin-bottom: 200px;
+    transition: 0.3;
+    cursor: pointer;
+    font-size: 18px;
+    margin-top: 50px;
+        :hover {
+            background-color: #FFF;
+            color: #00000F;
+            box-shadow: 0 0 5px #FFF;
+        }
+`;
+
+export const ButtonPreviousProject = styled.div`
+    background-color: #00000f;
+    border: 2px solid #FFF;
+    color: #FFF;
+    padding 20px 40px;
+    border-radius: 30px;
+    transition: 0.3;
+    cursor: pointer;
+    font-size: 18px;
+    margin-bottom: 50px;
+        :hover {
+            background-color: #FFF;
+            color: #00000F;
+            box-shadow: 0 0 5px #FFF;
+        }
+`;
+
+export const ProjectScreen = styled.div`
+    border: 1px solid #FFF;
+    width: 500px;
+    height: 300px;
+    margin: 20px;
+    color: white;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`;
+
+export const ProjectDescription = styled.div`
+    width: 500px;
+    height: 300px;
+    color: white;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    align-items: center;
+    text-align: justify;
+    font-size: 18px;
+    margin-left: 30px;
+`;
+
+export const ProjectTitle = styled.h2`
+    text-transform: uppercase;
+    margin: 0;
+    padding: 0;
+`;
+
+export const ProjectDescriptionText = styled.div`
+
+`;
+
+export const ProjectUsedTechnologies = styled.div`
+
 `;
 
 export const ContentPortfolio = styled.div`
-    display: block;
-    height: 100vh;
+    display: flex;
+    flex-direction: column;
     width: 100%;
     color: #000F;
-    background-color: #cff;
+    align-items: center;
+    padding-bottom: 200px;
 `;
 
 export const ContentContact = styled.div`
-    display: block;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
     height: 100vh;
     width: 100%;
-    color: #000F;
-    background-color: #ff4;
+    color: #000;
+    align-items: center;
+    padding-bottom: 200px;
 `;
 
-export const saudacao = styled.h1`
+export const CompContato = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 350px;
+    height: 500px;
+    border-radius: 20px;
+    border: 3px solid #FFF;
+    box-shadow: 0 0 3px #FFF;
+    transition: 0.3s;
+    justify-content: center;
+    background-color: #FFF;
+        :hover {
+            box-shadow: 0 0 20px #FFF;
+        }
+`;
+
+export const BarraSup = styled.div`
+    background-color: #075E54;
+    display: flex;
+    flex: 1;
+    border-radius: 20px 20px 0 0;
+    justify-content: space-between;
+    align-items: center;
+    padding: 5px;
+`;
+
+export const CompartSup = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 0 15px;
+`;
+
+export const SupIconDiv = styled.div`
+    width: 30px;
+    height: 30px;
+    border-radius: 15px;
+    filter: invert(100%);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+        :nth-child(2) {
+            margin-left: 10px;
+            transform: scale(0.9);
+        }
+`;
+
+export const SupIconImg = styled.img`
+    width: 80%;
+`;
+
+export const SupLogoImg = styled.img`
+    width: 80%;
+    filter: invert(100%);
+`;
+
+export const SupLogoDiv = styled.div`
+    width: 40px;
+    margin: 0 8px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: #FFF;
+    height: 40px;
+    border-radius: 20px;
+`;
+
+export const SupTextDiv = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    font-size: 13px;
+`;
+
+export const SupTextbold = styled.div`
+    font-weight: bold;
+    color: #FFF;
+`;
+
+export const SupTextNormal = styled.div`
+    color: #FFF;
+`;
+
+export const TelaConv = styled.div`
+    background-image: url(${whatsbg});
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    flex: 8;
+`;
+
+export const TelaCompSup = styled.div`
+    background-color: rgba( 255, 255, 255, 0.8);
+    margin-top: 30px;
+    margin-left: 5px;
+    width: 250px;
+    opacity: 0.8;
+    height: 40px;
+    border-radius: 10px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 5px 10px;
+`;
+
+export const telaConvTexto = styled.div`
+    color: #000;
+    font-size: 14px;
+`;
+
+export const linkWhatsapp = styled.a`
+    text-decoration: none;
+`;
+
+export const TelaConvHora = styled.div`
+    color: #000;
+    font-size: 9px;
+    margin-top: 30px;
+`;
+
+export const TelaCompInf = styled.div`
+    width: 100%;
+    height: 50px;
+    display: flex;
+    align-items: center;
+`;
+
+export const TelaInfType = styled.div`
+    flex: 1;
+    background-color: #FFF;
+    border-radius: 10px;
+    height: 40px;
+    margin-left: 10px;
+    margin-bottom: 10px;
+    box-shadow: 1px 1px 3px #FFF;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
+
+export const iconType = styled.div`
+    width: 35px;
+    height: 35px;
+    border-radius: 20px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+        :nth-child(3) {
+            transform: scale(0.8);
+        }
+`;
+
+export const iconTypeImg = styled.img`
+    width: 60%;
+    opacity: 0.5;
+`;
+
+export const textType = styled.div`
+    display: flex;
+    align-items: center;
+    font-size: 16px;
+    padding: 5px 0px;
+    opacity: 0.7;
+    margin: 0 5px;
+`;
+
+export const TelaInfRecord = styled.div`
+    width: 35px;
+    height: 35px;
+    border-radius: 20px;
+    background-color: #075E54;
+    margin-right: 10px;
+    margin-left: 10px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-bottom: 10px;
+`;
+
+export const recordImg = styled.img`
+    width: 60%;
+    filter: invert(100%);
+
+`;
+
+export const BarraInf = styled.div`
+    background-color: #000000;
+    display: flex;
+    flex: 1;
+    border-radius: 0 0 20px 20px;
+    align-items: center;
+    padding: 5px;
+    padding-left: 60px;
+`;
+
+export const divButtons = styled.div`
+    height: 30px;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: space-betweeen;
+        :nth-child(1) {
+            margin-left: 5px;
+        }
+        :nth-child(3) {
+            transform: scale(0.9);
+        }
+`;
+
+export const imgButtons = styled.img`
+    filter: invert(100%);
+    height: 80%;
+`;
+
+
+
+export const Whatsappdiv = styled.div`
+    width: 50px;
+    display: flex;
+    justify-content: center;
+    aling-items: center;
+    border-radius: 25px;
+    border-bottom-radius: 20px;
+`;
+
+export const WhatsappLogo = styled.img`
+    width: 100%;
+`;
+
+export const ContatoLink = styled.a`
+    text-decoration: none;
+    color: #000000;
+`;
+
+export const Typing = styled.h1`
     font-size: 50px;
     margin-top: 60px;
+    margin-bottom: 80px;
     text-align: center;
     overflow-x: hidden;
+    overflow-y: hidden;
     border-right: .15em solid rgb(249,168,38);
     white-space: nowrap;
     letter-spacing: .10em;
     animation: 
-    typing 2s steps(70, end),
+    typing 1s steps(70, end),
     blink-caret .75s step-end infinite;
     padding: 5px;
     color: #f9a826;
     cursor: text;
+
         @keyframes typing {
             from { width: 0 }
-            to { width: 87% }
+            to { width: 50% }
         }
         
         /* The typewriter cursor effect */
@@ -216,7 +578,7 @@ export const saudacao = styled.h1`
 `;
 
 export const Footer = styled.div`
-    padding: 30px;
+    padding: 20px;
     color: #FFF;
     text-align: center;
 `;
